f1f7d8654d34f33969de059c23f11c59e49ea43d
lipar2 <sukurmarinko4@gmail.com>
2024-05-23 15:43:51 +0200

    #2 stavljena dva atributa tipa integer

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,7 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Point extends Shape {
-
-	
+public class Point extends Shape{
+    private int x;
+    private int y;
+    private boolean selected=false;
 }
