2d01965c9ee735caee00051ba7966f8bb49ced3c
lipar2 <sukurmarinko4@gmail.com>
2024-05-23 16:02:41 +0200

    #2 komentar

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -2,20 +2,5 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Point extends Shape{
-    private int x;
-    private int y;
-private boolean selected= false;
-
- public Point (){}
-
- public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-}
-
-public Point(int x, int y, boolean selected) {
-    this.x = x;
-    this.y = y;
-    this.selected=selected;
-}
+   
 }
