ffcfa829f25ddb0e31f55930d4a34e8b73ea8bc0
lipar2 <sukurmarinko4@gmail.com>
2024-05-23 15:40:11 +0200

    #1 klasa point je podklasa klase shape

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,7 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
-   
 
-}
+    @Test
+    public void testGET() {
+        Point test1 = new Point(5, 7);
+        assertEquals(test1.getX(), 5);
+        assertEquals(test1.getY(), 7);
+
+    
+}}
