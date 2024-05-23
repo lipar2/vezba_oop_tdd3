d181376cc3e3432bbf163f9523350a52985e006d
lipar2 <sukurmarinko4@gmail.com>
2024-05-23 15:50:28 +0200

    #4  dodat prazan konstruktor

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,14 +1,13 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
 
     @Test
-    public void testGET() {
-        Point test1 = new Point(5, 7);
-        assertEquals(test1.getX(), 5);
-        assertEquals(test1.getY(), 7);
-
-    
-}}
+    public void test_prazanKonstruktor() {
+        Point test = new Point();
+        assertNotEquals(test, null);
+    }
+}
