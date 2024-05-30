e860395816489f17eb772ed1fcacc2ee6dfe2e5d
lipar2 <sukurmarinko4@gmail.com>
2024-05-30 07:09:22 +0200

    #4 klasa tacka sadrzi prazan konstruktor

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,2 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class PointTest {
+
+        @Test
+    public void test_prazanKonstruktor() {
+        Point test = new Point();
+        assertNotEquals(test, null);
+    }
+
+
 }
