21e9df7a24c25ec605dec2feb06a0095eed90b1a
lipar2 <sukurmarinko4@gmail.com>
2024-05-30 06:57:59 +0200

    #1 klasa je podklasa klase shape

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,21 +1,2 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import org.junit.jupiter.api.Test;
-
 public class PointTest {
-
-    @Test
-    public void test_prazanKonstruktor() {
-        Point test = new Point();
-        assertNotEquals(test, null);
-    }
-
-    @Test
-    public void KonstruktorTest(){
-        Point test = new Point(4,5,true);
-        assertEquals(test.getX(), 4);
-        assertEquals(test.getY(),5);
-        assertEquals(test.isSelected(), true);
-    }
 }
